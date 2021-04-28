5681eb5e87853e85a81ec30b6b8018ba48cd4a6a
Mei-Hui Su <mei@MeiPro.local>
2021-04-27 17:29:58 -0700

    first commit

---
+++ test/test_helper.h
@@ -0,0 +1,10 @@
+#ifndef TEST_HELPER_H
+#define TEST_HELPER_H
+
+#include "cvms.h"
+
+/* Execute cvms_txt as a child process */
+int run_cvms_txt(const char *cvmdir, const char *infile, const char *outfile);
+
+
+#endif
